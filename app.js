431bfb142ebbe1beb6ac911c9cf844fb97d2b67a
Ashley Coolman <ashley@yieldify.com>
2014-12-29 15:59:08 +0000

    added middleware: response time, logger, response

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 'use strict';
 
-var koa = require("koa");
+var koa = require('koa');
 var app = koa();
 
-// middleware: output
-app.use( function *(next) {
-    this.body = "Hello nurse!";
-})
+// mw: x response time
+app.use(function *(next) {
+    var t0 = new Date();
+    yield next;
+
+    var t1 = new Date();
+    this.set('x-response-time', (t1 - t0)+'ms');
+});
+
+// mw: logger
+app.use(function *(next) {
+    var t0 = new Date();
+    yield next;
+
+    var t1 = new Date();
+    console.log('%s %s (%sms)', this.method, this.url, t1-t0);
+});
+
+// mw: response
+app.use(function *() {
+    this.body = 'Hello nurse!';
+});
 
 // run
 app.listen(3001);
